320335bc0035c64be971c626dde9284ec431fa68
Marcelo Mata <www.escoladeginastica.com>
2024-11-18 09:31:16 -0300

    atualizacao dos cursos

--- assets/scripts/cursosTecnicos.js
+++ assets/scripts/cursosTecnicos.js
@@ -1,10 +1,10 @@
 let cursosTecnicos = [
 
-    // {id:1,enderecoHREF:'https://forms.gle/6C8HpYTSZDh4Vtw97',imgSRC:'./assets/img-outside/combo.jpg',alt:"Escolha seus cursos e monte seu combo" },
+    {id:1,enderecoHREF:'https://forms.gle/m8CLondZPLjadZtC7',imgSRC:'./assets/img-outside/cursosTecnicos/gymfriday.jpg',alt:"Escolha seus cursos e monte seu combo" },
     //{id:2,enderecoHREF:'https://go.hotmart.com/J80168536U',imgSRC:'./assets/img-outside/cursosTecnicos/FormacaoFinal.jpg',alt:"Curso Tecnico de Formação de Professores GAF"},
     {id:3,enderecoHREF:'https://hotmart.com/pt-br/marketplace/produtos/iniciacao-a-ginastica-artistica-feminina-aplicada-a-professores-como-comecar-curso-consultoria/L60248985M',imgSRC:'./assets/img-outside/cursosTecnicos/2.jpg', alt:"Iniciação à Ginástica Artística Feminina - Aplicada a professores - como começar? (Curso + Consultoria)"},
     {id:4,enderecoHREF:'https://go.hotmart.com/G71392175V',imgSRC:'./assets/img-outside/cursosTecnicos/nivel2.jpg',alt:"curso tecnico de ginastica artistica nivel 2"},    
-    {id:5,enderecoHREF:'https://go.hotmart.com/W74473283S',imgSRC:'./assets/img-outside/cursosTecnicos/5.jpg',alt:"arbitragem escolar gaf"},
+    {id:5,enderecoHREF:'https://go.hotmart.com/V96616072W',imgSRC:'./assets/img-outside/cursosTecnicos/ARBITRAGEM.jpg',alt:"cuso prearatorio de arbitragem gaf"},
     //{id:6,enderecoHREF:'https://hotmart.com/pt-br/marketplace/produtos/curso-preparatorio-de-arbitragem-ginastica-artistica-feminina-2022-2024/S60332014R',imgSRC:'./assets/img-outside/cursosTecnicos/3.jpg',alt:"Curso Preparatório de Arbitragem - Ginástica Artística Feminina 2022-2024"},
     {id:7,enderecoHREF:'https://go.hotmart.com/A77459330J',imgSRC:'./assets/img-outside/cursosTecnicos/ballet.jpg',alt:"Curso Online Básico de Ballet, Saltos de Dança e Giros - Ginástica Artística Feminina"},
     {id:8,enderecoHREF:'https://go.hotmart.com/L71346871M',imgSRC:'./assets/img-outside/cursosTecnicos/gr.jpg',alt:"curso iniciacao a GR"},
@@ -15,7 +15,7 @@ let cursosTecnicos = [
     {id:13,enderecoHREF:'https://go.hotmart.com/K79354458G',imgSRC:'./assets/img-outside/cursosTecnicos/BABIES.jpg',alt:"Oficina Ginástica na Prática - Ginástica para Babies 2 a 5 anos"},
     {id:14,enderecoHREF:'https://go.hotmart.com/O81911788F',imgSRC:'./assets/img-outside/cursosTecnicos/ESCOLA.jpg',alt:"Oficina Ginástica na Prática - Ginástica na Escola"},
     {id:15,enderecoHREF:'https://go.hotmart.com/O80874178W',imgSRC:'./assets/img-outside/cursosTecnicos/che.jpg',alt:"Oficina Cheerleading na Prática"},
-    //{id:16,enderecoHREF:'https://go.hotmart.com/J88588924J',imgSRC:'./assets/img-outside/cursosTecnicos/gg.jpg',alt:"Oficina Ginástica na Prática Prevenção de Lesões através do Treinamento de Força GA"},
+    {id:16,enderecoHREF:'https://go.hotmart.com/N96620809C',imgSRC:'./assets/img-outside/cursosTecnicos/paralelas.jpg',alt:"Oficina Ginástica na Prática paralelas assimetricas"},
     
     
 ]
